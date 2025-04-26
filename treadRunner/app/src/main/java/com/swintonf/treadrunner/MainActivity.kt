890778e1e6b62e9cce9878dcc0c83b195d2ac0fb
SwintonF <swintonafernandes@outlook.com>
2025-04-26 19:00:55 +0100

    Added heart rate button with no functionality

--- treadRunner/app/src/main/java/com/swintonf/treadrunner/MainActivity.kt
+++ treadRunner/app/src/main/java/com/swintonf/treadrunner/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.swintonf.treadrunner.ui.theme.TreadRunnerTheme
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +24,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TreadRunnerTheme {
-
+                ButtonLayout()
             }
         }
     }
 }
 
+@Composable
+fun ButtonLayout() {
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text("Heart Rate")
+        PermissionsButton { println("Code Ran") }
+    }
+}
+
+@Composable
+fun PermissionsButton(onClick: () -> Unit) {
+    Button(onClick = {onClick()}) {
+        Text("Button")
+    }
+}
