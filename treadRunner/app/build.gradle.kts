2826d9c0b8681a1a4b0c8256ebb916a5b6e67ecd
SwintonF <swintonafernandes@outlook.com>
2025-05-14 20:09:47 +0100

    build: change minSdk to 30

--- treadRunner/app/build.gradle.kts
+++ treadRunner/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.swintonf.treadrunner"
-        minSdk = 29
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
